db0baf5049dd594a82f70c686ccc92cdbbe84582
Ionut Simioniuc <Ionut.Simioniuc@endava.com>
2025-11-28 16:44:13 +0200

    Added class diagram. Added Prototype bean example in main.

--- CourseSpring/src/main/java/org/example/sq/part1/SmartHomeController/MainApp.java
+++ CourseSpring/src/main/java/org/example/sq/part1/SmartHomeController/MainApp.java
@@ -1,13 +1,20 @@
 package org.example.sq.part1.SmartHomeController;
 
 
+import org.example.sq.part1.SmartHomeController.services.lightning.LightControlServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainApp.class,args);
+        ConfigurableApplicationContext context = SpringApplication.run(MainApp.class,args);
+
+        LightControlServiceImpl service1 = context.getBean(LightControlServiceImpl.class);
+        LightControlServiceImpl service2 = context.getBean(LightControlServiceImpl.class);
+        System.out.println("Are beans equals ? " + (service1==service2) it + '\n'  + service1 +" - " + service2);
     }
 }
--- CourseSpring/src/main/java/org/example/sq/part1/SmartHomeController/configuration/ProjectConfiguration.java
+++ CourseSpring/src/main/java/org/example/sq/part1/SmartHomeController/configuration/ProjectConfiguration.java
@@ -3,6 +3,9 @@ package org.example.sq.part1.SmartHomeController.configuration;
 import org.example.sq.part1.SmartHomeController.aop.audit.LoggingAspect;
 import org.example.sq.part1.SmartHomeController.repository.lightRepository.LightInMemoryRepository;
 import org.example.sq.part1.SmartHomeController.repository.lightRepository.LightRepository;
+import org.example.sq.part1.SmartHomeController.services.lightning.LightControlServiceImpl;
+import org.springframework.aop.aspectj.annotation.PrototypeAspectInstanceFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
 
 @Configuration
@@ -20,4 +23,11 @@ public class ProjectConfiguration {
     public LoggingAspect logging(){
         return new LoggingAspect();
     }
+
+    @Bean
+    @Scope("prototype")
+    public LightControlServiceImpl lightControlServiceImpl(){
+        return new LightControlServiceImpl(lightRepository());
+    }
+
 }
